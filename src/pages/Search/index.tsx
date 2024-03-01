495a378ca6469e895986c974f9e3aea5111bc7b6
M4rcoos <contato.shopvmax@gmail.com>
2024-03-01 13:59:26 -0300

    feat: refact screen to striming

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,6 @@
 import { useRoute, RouteProp } from '@react-navigation/native';
 import { IArena } from '../../interfaces/IArena';
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import * as C from './styles'
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { Video, ResizeMode } from "expo-av";
@@ -18,21 +18,23 @@ type SearchScreenParams = {
 
 export function Search() {
   const route = useRoute<RouteProp<Record<string, SearchScreenParams>, string>>();
-  const responseEmpty = { "DatHora": "",
-   "DatProcessado": "", 
-   "DatUpload": "", 
-   "Frame": "", 
-   "HorarioVideoFrame": "", 
-   "NomArena": "", 
-   "NomExibicao": "", 
-   "NomQuadra": "", 
-   "play": "" }
+  const responseEmpty = {
+    "DatHora": "",
+    "DatProcessado": "",
+    "DatUpload": "",
+    "Frame": "",
+    "HorarioVideoFrame": "",
+    "NomArena": "",
+    "NomExibicao": "",
+    "NomQuadra": "",
+    "play": ""
+  }
   const [response, setResponse] = useState<IArenaVideo[]>([responseEmpty])
   const nomArena = route.params?.nomArena;
   const { favorites, setFavorites } = useContext(FavoriteContext)
 
   console.log(response[0])
-  
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -86,75 +88,73 @@ export function Search() {
   const video = useRef(null);
   const [status, setStatus] = useState({});
 
-  function areObjectsEqual(obj1: Record<string, any>, obj2: Record<string, any>): boolean  {
+  function areObjectsEqual(obj1: Record<string, any>, obj2: Record<string, any>): boolean {
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
-  
+
     if (keys1.length !== keys2.length) {
       return false;
     }
-  
+
     for (let key of keys1) {
       if (JSON.stringify(obj1[key]) !== JSON.stringify(obj2[key])) {
         return false;
       }
     }
-  
+
     return true;
   }
   return (
     <>
-   {
-   nomArena && !areObjectsEqual(response[0], responseEmpty) ? (
-    <C.Container>
-      <FlashList
-        data={response}
-        keyExtractor={(item) => String(item.HorarioVideoFrame)}
-        estimatedItemSize={1}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => {
-          const isFavorite = favorites.some((favVideo) => favVideo.play === item.play);
-          return (
-            <C.Content>
-              <Video
-                ref={video}
-                style={styles.video}
-                source={{ uri: item.play }}
-                focusable={true}
-                useNativeControls
-                resizeMode={ResizeMode.CONTAIN}
-                isLooping
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
-              />
-              <C.Description>
-                <View>
-                  <Text style={styles.local}>{item.NomExibicao}</Text>
-                  <Text style={styles.local}>{item.DatHora}</Text>
-                </View>
-                <C.Favorite onPress={() => isFavorite ? removeVideoFromFavorite(item) : addVideoToFavorite(item)}>
-                  {
-                    isFavorite ?
-                      <Entypo name="heart" size={24} color={theme.colors.green_700} />
-                      :
-                      <Entypo name="heart-outlined" size={24} color={theme.colors.green_700} />
-                  }
-                </C.Favorite>
-              </C.Description>
-            </C.Content>
-          )
-        }}
-      />
-    </C.Container>
-  )  : (
-   
-      <View style={styles.Container}>
-        <Text style={styles.text}>
-          Selecione uma quadra que tenha videos
-        </Text>
-      </View>
-    
-  )
-}
+      {
+        nomArena && !areObjectsEqual(response[0], responseEmpty) ? (
+          <C.Container>
+            <FlashList
+              data={response}
+              keyExtractor={(item) => String(item.HorarioVideoFrame)}
+              estimatedItemSize={1}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => {
+                const isFavorite = favorites.some((favVideo) => favVideo.play === item.play);
+                return (
+                  <C.Content>
+
+<View style={{ width: '60%', aspectRatio: 16/10, height: 200, backgroundColor:'#ccc'}}>
+  <Image
+    source={{ uri: item.Frame }}
+    style={{ width: "100%", height: "100%" }}  // "100%" em relação à largura do componente pai
+    resizeMode="contain"
+  />
+</View>
+                    <C.Description>
+                      <View>
+                        <Text style={styles.local}>{item.NomExibicao}</Text>
+                        <Text style={styles.local}>{item.DatHora}</Text>
+                      </View>
+                      <C.Favorite onPress={() => isFavorite ? removeVideoFromFavorite(item) : addVideoToFavorite(item)}>
+                        {
+                          isFavorite ?
+                            <Entypo name="heart" size={24} color={theme.colors.green_700} />
+                            :
+                            <Entypo name="heart-outlined" size={24} color={theme.colors.green_700} />
+                        }
+                      </C.Favorite>
+                    </C.Description>
+                  </C.Content>
+                )
+              }}
+            />
+          </C.Container>
+        ) : (
+
+          <View style={styles.Container}>
+            <Text style={styles.text}>
+              Selecione uma quadra que tenha videos
+            </Text>
+          </View>
+
+        )
+      }
     </>
   );
 }
